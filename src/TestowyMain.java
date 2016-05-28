d44568f223a788b87d0d70d6989ef4fa6fa87c8b
maltherii <unihilator@interia.eu>
2016-05-28 10:56:37 +0200

    Initial commit

---
+++ src/TestowyMain.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Maltherii on 28.05.2016.
+ */
+public class TestowyMain {
+
+    public static void printText(){
+        System.out.println("drukowanie jakiegos tekstu");
+    }
+    
+    public static void main(String args[]){
+        System.out.println("testowy");
+        printText();
+    }
+
+}
